ebd6aaf2d1da797b3e96f04075071cea9c14d12f
zahrotuladillah <zahrotuladillah@gmail.com>
2022-06-10 18:50:00 +0700

    add tugas1

---
+++ tugas1/progjar4a/.ipynb_checkpoints/file_client_cli-checkpoint.py
@@ -0,0 +1,118 @@
+import socket
+import json
+import base64
+import logging
+import time
+import os
+
+server_address=('0.0.0.0',7777)
+
+def send_command(command_str=""):
+    global server_address
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(server_address)
+    logging.warning(f"connecting to {server_address}")
+    try:
+        logging.warning(f"sending message ")
+        sock.sendall(command_str.encode())
+        # Look for the response, waiting until socket is done (no more data)
+        data_received="" #empty string
+        while True:
+            #socket does not receive all data at once, data comes in part, need to be concatenated at the end of process
+            data = sock.recv(16)
+            if data:
+                #data is not empty, concat with previous content
+                data_received += data.decode()
+                if "\r\n\r\n" in data_received:
+                    break
+            else:
+                # no more data, stop the process by break
+                break
+        # at this point, data_received (string) will contain all data coming from the socket
+        # to be able to use the data_received as a dict, need to load it using json.loads()
+        hasil = json.loads(data_received)
+        logging.warning("data received from server:")
+        print(hasil)
+        return hasil
+    except:
+        logging.warning("error during data receiving")
+        return False
+
+def remote_list():
+    command_str=f"LIST\n"
+    hasil = send_command(command_str)
+    if (hasil['status']=='OK'):
+        print("daftar file : ")
+        for nmfile in hasil['data']:
+            print(f"- {nmfile}")
+        return True
+    else:
+        print("Gagal")
+        return False
+
+def remote_get(filename=""):
+    command_str=f"GET {filename}\n"
+    hasil = send_command(command_str)
+    if (hasil['status']=='OK'):
+        #proses file dalam bentuk base64 ke bentuk bytes
+        namafile= hasil['data_namafile']
+        isifile = base64.b64decode(hasil['data_file'])
+        fp = open(namafile,'wb+')
+        fp.write(isifile)
+        fp.close()
+        return True
+    else:
+        print("Gagal")
+        return False
+    
+def remote_upload(filename=""):
+    simpan = open(filename, 'rb')
+   
+    data = base64.b64encode(simpan.read())
+    simpan.close()
+    command_str=f"upload {filename} {data}\n"
+    hasil = send_command(command_str)
+    if (hasil['status']=='OK'):
+        os.chdir("../")
+        history = open("history.txt", 'a')
+        localtime = time.asctime( time.localtime(time.time()) )
+        isi = f": file {filename} diupload \n"
+        history.write(localtime)
+        history.write(isi)
+        history.close()
+        print("Berhasil")
+        return True
+    else:
+        print("Gagal")
+        return False
+    
+def remote_delete(filename=""):
+    command_str=f"delete {filename}\n"
+    hasil = send_command(command_str)
+    if (hasil['status']=='OK'):
+        os.chdir("../")
+        history = open("history.txt", 'a')
+        localtime = time.asctime( time.localtime(time.time()) )
+        isi = f": file {filename} didelete \n"
+        history.write(localtime)
+        history.write(isi)
+        history.close()
+        print("Berhasil")
+        return True
+    else:
+        print("Gagal")
+        return False
+
+if __name__=='__main__':
+    server_address=('0.0.0.0',6667)
+    remote_list()
+    os.chdir("./upload_file")
+    # remote_upload('1.txt')
+    # remote_upload('3.txt')
+    # remote_upload('donalbebek.jpg')
+    remote_delete('donalbebek.jpg')
+    # remote_upload('rfc2616.pdf')
+    # remote_delete('rfc2616.pdf')
+    # remote_upload('2.py')
+    # remote_delete('1.txt')
+    #remote_get('pokijan.jpg')
---
+++ tugas1/progjar4a/file_server.py
@@ -0,0 +1,56 @@
+from socket import *
+import socket
+import threading
+import logging
+import time
+import sys
+
+from file_protocol import  FileProtocol
+fp = FileProtocol()
+
+class ProcessTheClient(threading.Thread):
+    def __init__(self, connection, address):
+        self.connection = connection
+        self.address = address
+        threading.Thread.__init__(self)
+
+    def run(self):
+        data_diterima =  ""
+        while True:
+            data = self.connection.recv(32)
+            if data:
+                data_diterima += data.decode()
+                if data_diterima[-1:]=='\n':  
+                    hasil = fp.proses_string(data_diterima)
+                    hasil=hasil+"\r\n\r\n"
+                    self.connection.sendall(hasil.encode())
+            else:
+                break
+        self.connection.close()
+
+class Server(threading.Thread):
+    def __init__(self,ipaddress='0.0.0.0',port=8889):
+        self.ipinfo=(ipaddress,port)
+        self.the_clients = []
+        self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        threading.Thread.__init__(self)
+
+    def run(self):
+        logging.warning(f"server berjalan di ip address {self.ipinfo}")
+        self.my_socket.bind(self.ipinfo)
+        self.my_socket.listen(1)
+        while True:
+            self.connection, self.client_address = self.my_socket.accept()
+            logging.warning(f"connection from {self.client_address}")
+
+            clt = ProcessTheClient(self.connection, self.client_address)
+            clt.start()
+            self.the_clients.append(clt)
+
+def main():
+    svr = Server(ipaddress='0.0.0.0',port=6667)
+    svr.start()
+
+if __name__ == "__main__":
+    main()
